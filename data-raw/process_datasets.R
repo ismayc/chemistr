619a1f8c110692727f60ce9ece21e37e7684d606
ismayc <chester.ismay@gmail.com>
2016-08-20 16:45:28 -0700

    Changed block to factor variable

--- data-raw/process_datasets.R
+++ data-raw/process_datasets.R
@@ -1,2 +1,4 @@
 periodic_table <- readr::read_csv(file = "data-raw/periodic_table_2016-08.csv")
+periodic_table<- dplyr::mutate(periodic_table,
+                               block = factor(block, levels = c("s", "p", "d", "f")))
 devtools::use_data(periodic_table, overwrite = TRUE)
