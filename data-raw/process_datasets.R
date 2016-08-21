497293d05f2fb32d79c6cb0371ee9347718fdfb0
ismayc <chester.ismay@gmail.com>
2016-08-20 18:48:23 -0700

    Removed trailing whitespace from block variable

--- data-raw/process_datasets.R
+++ data-raw/process_datasets.R
@@ -1,4 +1,5 @@
+trim.trailing <- function(x) sub("\\s+$", "", x)
+
 periodic_table <- readr::read_csv(file = "data-raw/periodic_table_2016-08.csv")
-periodic_table<- dplyr::mutate(periodic_table,
-                               block = factor(block, levels = c("s", "p", "d", "f")))
+periodic_table$block <- trim.trailing(periodic_table$block)
 devtools::use_data(periodic_table, overwrite = TRUE)
