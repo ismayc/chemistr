8751c16c77fc2c66fe346d7441ae235d57d8280e
ismayc <chester.ismay@gmail.com>
2016-08-22 21:11:15 -0700

    Updated Light template according to Danielle's specs

--- data-raw/process_datasets.R
+++ data-raw/process_datasets.R
@@ -2,4 +2,5 @@ trim.trailing <- function(x) sub("\\s+$", "", x)
 
 periodic_table <- readr::read_csv(file = "data-raw/periodic_table_2016-08.csv")
 periodic_table$block <- trim.trailing(periodic_table$block)
+readr::write_csv(periodic_table, "periodic-table-data.csv")
 devtools::use_data(periodic_table, overwrite = TRUE)
